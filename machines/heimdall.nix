3ea4009ab38c88b561fed231ba4af155898e46cd
Dominik Stodolny <dom@dmk.st>
2019-07-09 19:19:54 +0100

    improve synaptics configuration

--- machines/heimdall.nix
+++ machines/heimdall.nix
@@ -37,8 +37,23 @@
   hardware.bluetooth.enable = true;
   hardware.nitrokey.enable = true;
 
-  services.xserver.synaptics.enable = true;
-  services.xserver.synaptics.twoFingerScroll = true;
+  services.xserver.synaptics = {
+    enable = true;
+    twoFingerScroll = true;
+    horizTwoFingerScroll = true;
+    scrollDelta = -80;
+    palmDetect = true;
+    palmMinWidth = 4;
+    palmMinZ = 50;
+    minSpeed = "0.5";
+    maxSpeed = "1.0";
+    accelFactor = "0.075";
+    additionalOptions = ''
+      Option "MaxTapTime" "125"
+      Option "SoftButtonAreas" "93% 0 95% 0 0 0 0 0"
+    '';
+  };
+
   # Allow proprietary software
   nixpkgs.config.allowUnfree = true;
 }
