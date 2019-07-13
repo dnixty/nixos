ee2c567d106aca0f977198b8dc542735c2a8bb1e
Dominik Stodolny <dom@dmk.st>
2019-07-13 18:26:24 +0100

    add audio.nix and redshift.nix profiles

--- profiles/audio.nix
+++ profiles/audio.nix
@@ -1,6 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  sound = {
+    enable = true;
+    mediaKeys = {
+      enable = true;
+    };
+  };
+
   hardware.pulseaudio = {
     enable = true;
     package = pkgs.pulseaudioFull;
--- profiles/redshift.nix
+++ profiles/redshift.nix
@@ -5,7 +5,7 @@
     enable = true;
     latitude = "51.5094";
     longitude = "0.1365";
-    temperature.night = 4000;
+    temperature.night = 3000;
     brightness.night = "0.8";
   };
 }
