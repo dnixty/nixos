c2e850f5cd370d2a07f83fc86dc428bfd2341893
Dominik Stodolny <dom@dmk.st>
2019-07-13 18:21:03 +0100

    add brightness control keybindings

--- machines/heimdall.nix
+++ machines/heimdall.nix
@@ -50,4 +50,14 @@
 
   # Allow proprietary software
   nixpkgs.config.allowUnfree = true;
+
+  # Additional key bindings
+  programs.light.enable = true;
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+    ];
+  };
 }
