499cd63f63e4a0eaf722a2103241654964744631
Dominik Stodolny <dominik@stodolny.org>
2019-10-27 20:28:20 +0000

    fix grub version
    Signed-off-by: Dominik Stodolny <dominik@stodolny.org>

--- machines/njord.nix
+++ machines/njord.nix
@@ -5,7 +5,6 @@ let
   secrets = import ../secrets.nix;
 in
 {
-
   boot = {
     kernel = {
       sysctl."vm.overcommit_memory" = "1";
@@ -14,7 +13,7 @@ in
     loader = {
       grub.device = "/dev/vda";
       grub.enable = true;
-      grub.version = 1;
+      grub.version = 2;
     };
     cleanTmpDir = true;
   };
