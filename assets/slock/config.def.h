32ac08cb2120e08136b2fb2ae5c9cc2c18ae0fd8
Dominik Stodolny <dominik@stodolny.org>
2019-12-22 20:34:04 +0000

    change slock colours
    Signed-off-by: Dominik Stodolny <dominik@stodolny.org>

--- assets/slock/config.def.h
+++ assets/slock/config.def.h
@@ -4,8 +4,8 @@ static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#0a3749",   /* during input */
-	[FAILED] = "#c23127",   /* wrong password */
+	[INPUT] =  "#458588",   /* during input */
+	[FAILED] = "#cc241d",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
