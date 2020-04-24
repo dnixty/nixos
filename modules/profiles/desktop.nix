23703cb245eb91363005ae41fba2094ad87f7641
Dominik Stodolny <dominik@stodolny.org>
2020-04-24 18:24:15 +0100

    autologin on tty1 and startx

--- modules/profiles/desktop.nix
+++ modules/profiles/desktop.nix
@@ -3,7 +3,6 @@
 with lib;
 let
   cfg = config.profiles.desktop;
-  shared = import ../../shared.nix;
   my-slock = pkgs.slock.override { conf = builtins.readFile ../../assets/slock/config.def.h; };
 in
 {
@@ -19,18 +18,12 @@ in
   };
   config = mkIf cfg.enable {
     profiles.audio.enable = cfg.audio;
-    services.xserver = {
-      enable = true;
-      autoRepeatDelay = 250;
-      autoRepeatInterval = 30;
-      xkbOptions = "ctrl:swapcaps";
-      displayManager = {
-        sessionCommands = ''
-          ${pkgs.xss-lock}/bin/xss-lock slock &
-        '';
-        auto = {
-          enable = true;
-          user = shared.user.username;
+    services = {
+      xserver = {
+        enable = true;
+        xkbOptions = "ctrl:swapcaps";
+        displayManager = {
+          startx.enable = true;
         };
       };
     };
